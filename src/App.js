0e274683320878ac72dc03c764b766e9c04b00e9
Chrissa Agujar <55868590+cagujar@users.noreply.github.com>
2023-04-28 22:31:15 +0800

    calculator ui design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from "./components/Button";
+import Input from "./components/Input";
 
-function App() {
+const App = () => {
+  const buttonColor = "##D1A6CC"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='calc-wrapper'>
+        <Input />
+        <div className='row'>
+          <Button symbol={"7"} /> 
+          <Button symbol={"8"}/> 
+          <Button symbol={"9"}/>
+          <Button symbol={"/"} color={buttonColor}/>
+        </div>
+        <div className='row'>
+          <Button symbol={"4"}/> 
+          <Button symbol={"5"}/> 
+          <Button symbol={"6"}/>
+          <Button symbol={"-"}  color={buttonColor}/>
+        </div>
+        <div className='row'>
+          <Button symbol={"1"}/> 
+          <Button symbol={"2"}/> 
+          <Button symbol={"3"}/>
+          <Button symbol={"+"}  color={buttonColor}/>
+        </div>
+        <div className='row'>
+          <Button symbol={"0"}/> 
+          <Button symbol={"."}/> 
+          <Button symbol={"="}/>
+          <Button symbol={"x"}  color={buttonColor}/>
+        </div>
+        <Button symbol={"clear"}/>
+      </div>
     </div>
   );
 }
---
+++ src/components/Input.js
@@ -0,0 +1,14 @@
+import "./Input.css"
+
+const Input = () => {
+    return <div className="input-wrapper"> 
+        <div className="result">
+            <h1>Result</h1>
+        </div>
+        <div className="text">
+            <h3>Text</h3>
+        </div>
+    </div>
+}
+
+export default Input;
